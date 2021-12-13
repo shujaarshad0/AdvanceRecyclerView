b23319b2025bd860d8ca531cdcf7521c55c015bf
Mohammad.Shuja <Mohammad.Shuja@centegytechnologies.com>
2021-12-13 14:16:24 +0500

    Work for all screen sizes

--- app/src/main/java/com/shuja/recyclerviewdragndropitem/MainActivity.java
+++ app/src/main/java/com/shuja/recyclerviewdragndropitem/MainActivity.java
@@ -2,6 +2,7 @@ package com.shuja.recyclerviewdragndropitem;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
@@ -11,6 +12,7 @@ import android.view.MenuItem;
 import android.view.animation.BounceInterpolator;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.Toolbar;
@@ -24,14 +26,18 @@ import com.google.android.material.snackbar.Snackbar;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements OnStartDragListener {
+public class MainActivity extends AppCompatActivity implements OnStartDragListener, View.OnTouchListener {
 
-    RecyclerView mRecyclerView;
-    ItemTouchHelper mItemTouchHelper;
-    boolean isOpened = false;
+    private RecyclerView mRecyclerView;
+    private ItemTouchHelper mItemTouchHelper;
+    private boolean isOpened = false;
+    private FloatingActionButton fab;
+    private AppCompatButton subtract_button, add_button;
+    private View touch_view;
+    private float addButtonWidth, subtractButtonWidth;
 
-    private View.OnTouchListener handleTouch;
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,60 +46,27 @@ public class MainActivity extends AppCompatActivity implements OnStartDragListen
         setSupportActionBar(toolbar);
         mRecyclerView = findViewById(R.id.recycler_view);
 
-        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        final AppCompatButton subtract_button = (AppCompatButton) findViewById(R.id.subtract_button);
-        final AppCompatButton add_button = (AppCompatButton) findViewById(R.id.add_button);
-        final View touch_view = (View) findViewById(R.id.touch_view);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        subtract_button = (AppCompatButton) findViewById(R.id.subtract_button);
+        add_button = (AppCompatButton) findViewById(R.id.add_button);
+        touch_view = (View) findViewById(R.id.touch_view);
 
-        handleTouch = new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    if (isOpened) {
-                        touch_view.setVisibility(View.GONE);
-                        add_button.animate().translationX(0).setDuration(100).setInterpolator(null);
-                        subtract_button.animate().translationX(0).setDuration(200).setInterpolator(null);
-                        new Handler().postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
-                                add_button.setVisibility(View.GONE);
-                                subtract_button.setVisibility(View.GONE);
-                            }
-                        }, 200);
+        //Getting the width of views that have to be animated
+        //In our case the add and subtract buttons width isn 50 dp
+        //Therefore we convert dp to pixels for different screen sizes support
+        addButtonWidth = convertDpToPx(50f);
+        subtractButtonWidth = convertDpToPx(50f);
 
-                    }
-                    isOpened = !isOpened;
-                }
-                return true;
-            }
-        };
-
-        touch_view.setOnTouchListener(handleTouch);
-
+        touch_view.setOnTouchListener(this);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (isOpened) {
-                    touch_view.setVisibility(View.GONE);
-                    add_button.animate().translationX(0).setDuration(100).setInterpolator(null);
-                    subtract_button.animate().translationX(0).setDuration(200).setInterpolator(null);
-                    new Handler().postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            add_button.setVisibility(View.GONE);
-                            subtract_button.setVisibility(View.GONE);
-                        }
-                    }, 200);
-
+                    closeQuantityBox();
                 } else {
-                    touch_view.setVisibility(View.VISIBLE);
-                    add_button.setVisibility(View.VISIBLE);
-                    subtract_button.setVisibility(View.VISIBLE);
-                    add_button.animate().translationX(-340).setDuration(400).setInterpolator(new CustomBounce(0.3, 10));
-                    subtract_button.animate().translationX(-170).setDuration(200).setInterpolator(new CustomBounce(0.3, 10));
+                    openQuantityBox();
                 }
                 isOpened = !isOpened;
-
             }
         });
 
@@ -112,17 +85,63 @@ public class MainActivity extends AppCompatActivity implements OnStartDragListen
         mRecyclerView.setAdapter(mAdapter);
 
 
+        //For hiding the view on recycler scroll
         mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
             public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
                 super.onScrollStateChanged(recyclerView, newState);
                 if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    //Translate to zero for bringing views back to their original positions
                     fab.animate().translationX(0).setDuration(300);
                 } else {
-                    fab.animate().translationX(200).setDuration(300);
+                    //We've given 60 in translationX for fully hiding the view
+                    //50dp is width of button and 10dp is margin, so it 60dp
+                    fab.animate().translationX(convertDpToPx(60)).setDuration(300);
                 }
             }
         });
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            if (isOpened) {
+                closeQuantityBox();
+            }
+            isOpened = !isOpened;
+        }
+        return true;
+    }
+
+    public void openQuantityBox() {
+        touch_view.setVisibility(View.VISIBLE);
+        add_button.setVisibility(View.VISIBLE);
+        subtract_button.setVisibility(View.VISIBLE);
+
+        //Negate(-) the value of with for animating the view from LTR
+        add_button.animate().translationX(-(addButtonWidth + subtractButtonWidth)).setDuration(400).setInterpolator(new CustomBounce(0.3, 10));
+        subtract_button.animate().translationX(-subtractButtonWidth).setDuration(200).setInterpolator(new CustomBounce(0.3, 10));
+    }
+
+    public void closeQuantityBox() {
+        touch_view.setVisibility(View.GONE);
+
+        //Translate to zero for bringing views back to their original positions
+        add_button.animate().translationX(0).setDuration(100).setInterpolator(null);
+        subtract_button.animate().translationX(0).setDuration(200).setInterpolator(null);
+
+        //Delay is added so that the animating items should be invisible when the animation is done
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                add_button.setVisibility(View.GONE);
+                subtract_button.setVisibility(View.GONE);
+            }
+        }, 200);
+    }
+
+    public float convertDpToPx(float dp) {
+        return dp * getResources().getDisplayMetrics().density;
     }
 
     @Override
